31be6356397e1b0b6d9ec768119baab4f535618c
alexisrom <romeroalexis1994@gmail.com>
2018-08-17 01:24:06 -0300

    Se elimina Login de clientes

--- SIGESVI/SIGESVI/Presentacion/Ventanas/FrmBienvenida.vb
+++ SIGESVI/SIGESVI/Presentacion/Ventanas/FrmBienvenida.vb
@@ -14,7 +14,7 @@
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Dim frm = New login2()
+        Dim frm = New FrmCatalogo()
         frm.ShowDialog()
         Me.Close()
     End Sub

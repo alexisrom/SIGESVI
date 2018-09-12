c4433c4f01ac03a7ca9678aabdc73b4ce818103f
alexisrom <romeroalexis1994@gmail.com>
2018-09-12 14:14:57 -0300

    Datos de prueba

---
+++ sql/datos_prueba.sql
@@ -0,0 +1,12 @@
+INSERT INTO sucursal (nombre, direccion, departamento) VALUES
+("Sucursal 1", "Calle 1", "Montevideo"),
+("Sucursal 2", "Calle 2", "Canelones"),
+("Sucursal 3", "Calle 3", "Maldonado"),
+("Sucursal 4", "Calle 4", "Durazno");
+
+INSERT INTO telefono_sucursal (id_sucursal, telefono) VALUES
+(1, 1111111),
+(1, 1111112),
+(2, 2222222),
+(3, 3333333),
+(4, 1111111);
